884b3f92c7ce6ec99176e1748c74450fed99cbc0
Chong, Jane <jane.chong@hilti.com>
2024-10-11 11:16:56 +0800

    Creating post requests and setting tab logic

--- index.js
+++ index.js
@@ -10,16 +10,63 @@ const port = 3000;
 
 app.use(express.static(__dirname + "/public"));
 
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 // handling requests
+app.get("/", (req, res) => {
+    const page = "home"
+    res.render("blog-app.ejs", {
+        pageTitle : page
+    });
+})
+
 app.get("/design-dump", (req, res) => {
     res.render("index.ejs");
 });
 
-app.get("/", (req, res) => {
-    res.render("blog-app.ejs");
+// posting for tabs
+
+app.post("/home", (req, res) => {
+    const page = "home"
+    res.render("blog-app.ejs", {
+        pageTitle : page
+    });
+});
+
+app.post("/notifications", (req, res) => {
+    const page = "notifications"
+    res.render("blog-app.ejs", {
+        pageTitle : page
+    });
+});
+
+app.post("/bookmarks", (req, res) => {
+    const page = "bookmarks"
+    res.render("blog-app.ejs", {
+        pageTitle : page
+    });
+});
+
+app.post("/vault", (req, res) => {
+    const page = "vault"
+    res.render("blog-app.ejs", {
+        pageTitle : page
+    });
+});
+
+app.post("/write", (req, res) => {
+    const page = "write"
+    res.render("blog-app.ejs", {
+        pageTitle : page
+    });
+});
+
+app.post("/settings", (req, res) => {
+    const page = "settings"
+    res.render("blog-app.ejs", {
+        pageTitle : page
+    });
 });
 
 app.listen(port, () => {
